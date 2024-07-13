ff563107fea87668148bed0eeb4b8266a75a9f16
Elena <olena168@gmail.com>
2024-07-13 12:13:45 -0400

    updated Auth

--- src/index.js
+++ src/index.js
@@ -1,20 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { Auth0Provider } from '@auth0/auth0-react';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { Auth0Provider } from "@auth0/auth0-react";
 
-import { configureStore } from '@reduxjs/toolkit';
-import { Provider } from 'react-redux';
+import { configureStore } from "@reduxjs/toolkit";
+import { Provider } from "react-redux";
 
-
-import candleReducer, { candleFetch } from './features/productsSlice';
-import { productsApi } from './features/productsApi';
-import cartReducer, { getTotals } from './features/cartSlice';
-
+import candleReducer, { candleFetch } from "./features/productsSlice";
+import { productsApi } from "./features/productsApi";
+import cartReducer, { getTotals } from "./features/cartSlice";
 
 const store = configureStore({
-  reducer:{
+  reducer: {
     candle: candleReducer,
     cart: cartReducer,
     [productsApi.reducerPath]: productsApi.reducer,
@@ -24,26 +22,25 @@ const store = configureStore({
   },
 });
 
-store.dispatch(candleFetch());// from productsSlice
+store.dispatch(candleFetch()); // from productsSlice
 store.dispatch(getTotals());
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Auth0Provider
-    domain='dev-0bzyn8oh7pins32g.us.auth0.com'
-    clientId='YCiUtNVQ73fg1EHvKKsbn5oHPTOcRdpj'
-    authorizationParams={{
-      redirect_uri: window.location.origin
-    }}
+      domain="dev-0bzyn8oh7pins32g.us.auth0.com"
+      clientId="48zNlCrv7r0N2j5fRHI7ZKVcKTmtcRVy"
+      authorizationParams={{
+        redirect_uri: window.location.origin,
+      }}
     >
-    <Provider store={store}>
-    <App />
-    </Provider>
+      <Provider store={store}>
+        <App />
+      </Provider>
     </Auth0Provider>
   </React.StrictMode>
 );
-
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
